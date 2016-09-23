9c55c6e9670c99e660b0c86a26b742cd0b18591c
Chao Xu <xux1912@gmail.com>
2016-09-23 10:25:08 -0500

    Create add_header.sh

---
+++ add_header.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################
+## Add header $1 to file $2
+## For example, add header "Chr Gene nSNPs" to file test.txt
+##   Using ./add_header.sh "Chr Gene nSNPS" test.txt
+###############################################################
+
+echo $1 > file_$1.tmp
+cat $2 >> file_$1.tmp
+mv file_$1.txt $2
