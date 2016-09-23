e7199df71217495f258996d31896eafb6c9db765
Chao Xu <xux1912@gmail.com>
2016-09-23 10:28:10 -0500

    Update add_header.sh

--- add_header.sh
+++ add_header.sh
@@ -4,8 +4,9 @@
 ## Add header $1 to file $2
 ## For example, add header "Chr Gene nSNPs" to file test.txt
 ##   Using ./add_header.sh "Chr Gene nSNPS" test.txt
+## Use "\t" if the delimiter is tab, like "Chr\tGene\tnSNPs"
 ###############################################################
 
-echo $1 > file_$1.tmp
+echo -e $1 > file_$1.tmp
 cat $2 >> file_$1.tmp
 mv file_$1.tmp $2
