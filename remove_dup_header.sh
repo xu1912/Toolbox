18e491d8b2a42e9193394529b758329d5f9d6908
Chao Xu <xux1912@gmail.com>
2016-10-05 10:47:53 -0500

    Update remove_dup_header.sh

--- remove_dup_header.sh
+++ remove_dup_header.sh
@@ -3,8 +3,7 @@
 ##If we combine several files with header into a single file using command cat, duplicated headers will be stored in the new file. Use this command to remove them and only keep the first one.
 ## Required parameters: $1 -- file name; $2 -- keyword to recognize the header
 
-
-head -n1 $1 > tmp_$1.out
-sed -i "/$2/d" $i
-cat $i >> tmp_$1.out
-mv tmp_$1.out $1
+head -n1 $1 > tmp_$1
+sed -i /$2/d $1
+cat $1 >> tmp_$1
+mv tmp_$1 $1
