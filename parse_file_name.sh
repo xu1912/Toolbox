75a9ddf3b9dc882cc8df90ad5a23917340024cac
Chao Xu <xux1912@gmail.com>
2016-10-14 15:53:09 -0500

    Update parse_file_name.sh

--- parse_file_name.sh
+++ parse_file_name.sh
@@ -1,14 +1,15 @@
-If you want a bash-only solution, you can start with:
+#If you want a bash-only solution, you can start with:
 
-pax> a=/tmp/xx/file.tar.gz
-pax> xpath=${a%/*} 
-pax> xbase=${a##*/}
-pax> xfext=${xbase##*.}
-pax> xpref=${xbase%.*}
-pax> echo;echo path=${xpath};echo pref=${xpref};echo ext=${xfext}
+a=/tmp/xx/file.tar.gz
+xpath=${a%/*} 
+xbase=${a##*/}
+xfext=${xbase##*.}
+xpref=${xbase%.*}
+echo;echo path=${xpath};echo pref=${xpref};echo ext=${xfext}
 
-path=/tmp/xx
-pref=file.tar
-ext=gz
+## Result:
+## path=/tmp/xx
+## pref=file.tar
+## ext=gz
 
-That little snippet sets xpath (the file path), xpref (the file prefix) and xfext (the file extension).
+##That little snippet sets xpath (the file path), xpref (the file prefix) and xfext (the file extension).
